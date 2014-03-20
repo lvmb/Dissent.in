12a39c0c15257fd196b725b9908efaf356f1f1cd
lvmb <a65bdb43@opayq.com>
2014-03-20 16:12:20 +0000

    Update tweets.py

--- tweets.py
+++ tweets.py
@@ -1,21 +1,47 @@
 import sqlite3
 
-def main(cursor):
-    cursor.execute("select * from tweets")
-    for tweet in cursor.fetchall():
-        tid = tweet[0]
-        tdate = tweet[1]
-        text = tweet[2]
-        geo = tweet[3]
-        keyword = tweet[4]
-        
-        print '-----tweet: %s ' % text
-        print '------date: %s'  % tdate
-        print '-------geo: %s'  % geo
-        print '----length: %s' % len(text)
-        print '--keywords: %s' % keyword
-        print '\n'
+def count_tweets(cursor, cmd):
+	lines = 0
+	lst = list()
+	cursor.execute(cmd)
+	for tweet in cursor.fetchall():
+		lst.append(tweet)
+		lines += 1
+	return lines	
 
+def build_tweet(cursor, tweet):
+	t_id = 'Tweet ID: %s' % tweet[0]
+	t_date = 'Tweet Date: %s' % tweet[1]
+	t_text = 'Tweet Text: %s' % tweet[2]
+	t_geo = 'Tweet Location: %s' % tweet[3]
+	t_keyword = 'Tweet Keyword: %s' % tweet[4]
+	t = t_id + '\n' + t_date + '\n' + t_text + '\n' + t_keyword + '\n'
+	return t	
+
+def show_tweets(cursor):
+	cmd = "select * from tweets"
+	cursor.execute(cmd)
+	for tweet in cursor.fetchall():
+		print build_tweet(cursor, tweet)	    
+	print '-----All Tweets: %s' % count_tweets(cursor, cmd)
+
+def show_tweet_count(cursor):
+	cmd = "select * from tweets"
+	print '-----All Tweets: %s' % count_tweets(cursor, cmd)
+
+
+def filter_by_keyword(cursor):
+ 	keyword = raw_input('Keyword > ')
+ 	t = (keyword, )
+ 	cursor.execute("select * from tweets where keyword = ?", t)
+ 	for tweet in cursor.fetchall():
+ 		print build_tweet(cursor, tweet)
+
+def main(cursor):
+
+	show_tweets(cursor)
+	filter_by_keyword(cursor)
+	
 if __name__ == '__main__':
     conn = sqlite3.connect('tweets.db')
     conn.text_factory = str
